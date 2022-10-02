710a136bfcee967c554b3ae3198f0e52d3db0120
DheaSabillaa <111202214102@mhs.dinus.ac.id>
2022-10-02 21:01:42 +0700

    Minggu 4

---
+++ minggu4/form.php
@@ -0,0 +1,31 @@
+<?php
+$nisn=$_POST['nisn'];
+$asalsekolah=$_POST['asalsekolah'];
+$tahunlulus=$_POST['tahunlulus'];
+$nama=$_POST['nama'];
+$nik=$_POST['nik'];
+$tempatlahir=$_POST['tempatlahir'];
+$tanggal=$_POST['tanggal'];
+$agama=$_POST['agama'];
+$alamat=$_POST['alamat'];
+$emailaktif=$_POST['emailaktif'];
+$nomerhp=$_POST['nomerhp'];
+$jk=$_POST['jk'];
+$hobi=$_POST['hobi'];
+$foto=$_POST['foto'];
+
+echo "NISN : ".$nisn."<br>";
+echo "Asal Sekolah : ".$asalsekolah."<br>";
+echo "Tahun Lulus : ".$tahunlulus."<br>";
+echo "Nama : ".$nama."<br>";
+echo "NIK : ".$nik."<br>";
+echo "Tempat Lahir : ".$tempatlahir."<br>";
+echo "Tanggal Lahir : ".$tanggal."<br>";
+echo "Agama : ".$agama."<br>";
+echo "Alamat : ".$alamat."<br>";
+echo "Email Aktif : ".$emailaktif."<br>";
+echo "Nomor HP : ".$nomerhp."<br>";
+echo "Jenis Kelamin : ".$jk."<br>";
+echo "Hobi : ".$hobi."<br>";
+echo "File : ".$foto."<br>";
+?>
